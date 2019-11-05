983f51f73455f63ae359cef5cb934d0db6670b42
imkiva <libkernelpanic@gmail.com>
2019-11-05 23:48:14 +0800

    main: unnamed class object

--- src/main/java/com/imkiva/playground/Main.java
+++ src/main/java/com/imkiva/playground/Main.java
@@ -1,10 +1,26 @@
 package com.imkiva.playground;
 
+import com.imkiva.playground.reflection.core.Reflector;
+
 /**
  * @author kiva
  * @date 2019-10-09
  */
 public class Main {
-    public static void main(String[] args) {
+    private void fuck() {
+    }
+
+    private static Object make(int base) {
+        return new Object() {
+            public int add(int x) {
+                return x + base;
+            }
+        };
+    }
+
+    public static void main(String[] args) throws Throwable {
+        var x = make(10);
+        int a = Reflector.of(x).call("add", 1).get();
+        System.out.println(a);
     }
 }
