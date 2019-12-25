60d7800cb292ba81afcf0f33e87548965d99fc74
imkiva <libkernelpanic@gmail.com>
2019-12-25 11:26:21 +0800

    toys: magic xor

--- src/main/java/com/imkiva/playground/toys/MagicXor.java
+++ src/main/java/com/imkiva/playground/toys/MagicXor.java
@@ -1,23 +1,32 @@
 package com.imkiva.playground.toys;
 
+import java.util.Scanner;
+
 /**
  * @author kiva
  * @date 2019/12/2
  */
 public class MagicXor {
+    private static boolean verify(String s) {
+        var ref = new Object() {
+            int index = 0;
+        };
+
+        int x = s.chars()
+                .limit(17)
+                .map((c) -> (1 << (18 - ref.index++ - 1)) % 11 * (c - '0'))
+                .reduce(0, Integer::sum);
+
+        x = (12 - (x % 11)) % 11;
+        return (x + '0') == s.charAt(17);
+    }
+
     public static void main(String[] args) {
-        int[] ints = new int[233];
-        for (int i = 0; i < 233; i++) {
-            ints[i] = i;
+        String s = new Scanner(System.in).nextLine();
+        if (verify(s)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("FUCK");
         }
-
-        ints[222] = 0;
-
-        int result = 0;
-        for (int i = 0; i < ints.length; i++) {
-            result ^= i ^ ints[i];
-        }
-
-        System.out.println(result);
     }
 }
